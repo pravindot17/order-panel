35b5e8a13f3d81b95557db801f378e5a64033c28
Pravin Lolage <pravindot17@gmail.com>
2019-08-10 19:48:24 +0530

    addded static products

--- src/app/order-add/order-add.component.ts
+++ src/app/order-add/order-add.component.ts
@@ -17,7 +17,13 @@ export class OrderAddComponent implements OnInit {
   products = [];
   isLoadingResults = false;
 
-  staticProducts = [{ name: 'Kingston 32 Gb Pendrive', amount: 450 }];
+  staticProducts = [
+    { name: 'Kingston 32 Gb Pendrive', amount: 450 },
+    { name: 'FlyBot Truly Wireless', amount: 2750 },
+    { name: 'Sony XB450 Headphone', amount: 1500 },
+    { name: 'Lenovo G40 laptop', amount: 27000 },
+    { name: 'Timex S90', amount: 1750 },
+  ];
 
   constructor(private router: Router, private api: OrderService, private formBuilder: FormBuilder) { }
 
